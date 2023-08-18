03ecda723f460844367f1b476060295185e443d6
parsme <marzieh.nokhbeh@gmail.com>
2023-08-18 12:38:12 +0330

    delete subscription

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -10,7 +10,7 @@ import Button from "@/components/Button";
 const AccountContent = () => {
   const router = useRouter();
  
-  const { isLoading, subscription, user } = useUser();
+  const { isLoading, user } = useUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -24,23 +24,10 @@ const AccountContent = () => {
 
   return ( 
     <div className="mb-7 px-6">
-      {!subscription && (
+      
+     
         <div className="flex flex-col gap-y-4">
-        <p>No active plan.</p>
-        <Button 
-          //onClick={subscribeModal.onOpen}
-          className="w-[300px]"
-        >
-          Subscribe
-        </Button>
-      </div>
-      )}
-      {subscription && (
-        <div className="flex flex-col gap-y-4">
-          <p>You are currently on the 
-            <b> {subscription?.prices?.products?.name} </b> 
-            plan.
-          </p>
+         
           <Button
             disabled={loading || isLoading}
             //onClick={redirectToCustomerPortal}
@@ -49,7 +36,7 @@ const AccountContent = () => {
             Open customer portal
           </Button>
         </div>
-      )}
+      
     </div>
   );
 }
--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -36,33 +36,33 @@ export const MyUserContextProvider = (props: Props) => {
   const [subscription, setSubscription] = useState<Subscription | null>(null);
 
   const getUserDetails = () => supabase.from('users').select('*').single();
-  const getSubscription = () =>
+  /* const getSubscription = () =>
     supabase
       .from('subscriptions')
       .select('*, prices(*, products(*))')
       .in('status', ['trialing', 'active'])
-      .single();
+      .single(); */
 
   useEffect(() => {
-    if (user && !isLoadingData && !userDetails && !subscription) {
+    if (user && !isLoadingData && !userDetails /* && !subscription */) {
       setIsloadingData(true);
-      Promise.allSettled([getUserDetails(), getSubscription()]).then(
+      Promise.allSettled([getUserDetails()/* , getSubscription() */]).then(
         (results) => {
           const userDetailsPromise = results[0];
-          const subscriptionPromise = results[1];
+          //const subscriptionPromise = results[1];
 
           if (userDetailsPromise.status === 'fulfilled')
             setUserDetails(userDetailsPromise.value.data as UserDetails);
 
-          if (subscriptionPromise.status === 'fulfilled')
-            setSubscription(subscriptionPromise.value.data as Subscription);
+          /* if (subscriptionPromise.status === 'fulfilled')
+            setSubscription(subscriptionPromise.value.data as Subscription); */
 
           setIsloadingData(false);
         }
       );
     } else if (!user && !isLoadingUser && !isLoadingData) {
       setUserDetails(null);
-      setSubscription(null);
+      //setSubscription(null);
     }
   }, [user, isLoadingUser]);
 
